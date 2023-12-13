380bc843b2412bb3ebbf941f965ad7d6bc91770e
tuan anh <tuananhtran.xd@gmail.com>
2023-12-13 21:39:44 +0700

    first commit

---
+++ TestCircle.java
@@ -0,0 +1,7 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(10);
+        System.out.println(c1.getArea());
+        System.out.println(c1.getRadius());
+    }
+}
